d92288e354cd06758bd62109ad46d6fdfb1e54d2
unknown <sxcueto@gmail.com>
2023-06-01 15:12:16 -0400

    push for generator

--- src/components/About.js
+++ src/components/About.js
@@ -1,42 +1,35 @@
 import React from "react";
-import { FaArrowDown, FaArrowUp, FaLinkedin, FaLink, FaGithub, FaDiceD20 } from 'react-icons/fa';
-import Button from 'react-bootstrap/Button';
+import { FaArrowDown, FaArrowUp, FaLinkedin, FaLink, FaGithub } from 'react-icons/fa';
+import FunFact from './FunFact';
 
-const About = () =>{
-return (
-    <div>
-        {/* //section one "about" landing page */}
-        <section id="section-one">
-            <header className="container-fluid" id="jumbo">
-                <div className="container-fluid text-center text-black">
-                    <br />
-                    <h1 className="display-3" id="about-header"></h1>
-                    <a href="https://www.artworkscincinnati.org/mural/the-hands-that-built-our-city/" target="_blank" rel="noopener noreferrer"><Button variant="info"><i class="bi bi-info-lg"></i></Button></a>
-                    <br />
-                    <a href="/about#section-two">
-                        <FaArrowDown className="icons" />
+const About = () => {
+    return (
+        <div className="scroll-container">
+            {/* //section one "about" landing page */}
+            <section id="section-one">
+                <header className="container-fluid" id="jumbo">
+                    <div className="container-fluid text-center">
+                        <br />
+                        <a href="/about#section-two">
+                            <FaArrowDown className="icons" />
+                        </a>
+                    </div>
+                </header>
+            </section>
+            <br />
+
+            { /* section two: info about Cincy */}
+            <section id="section-two">
+                <div className="container-fluid text-center" id="content-two">
+                    <a href="/about#section-one">
+                        <FaArrowUp className="icons" />
                     </a>
-                </div>
-            </header>
-        </section>
-        <br />
-        { /* section two: info about Cincy */}
-        <section id="section-two">
-            <div className="container-fluid text-center" id="content-two">
-                <a href="/about#section-one">
-                    <FaArrowUp className="icons"/>
-                </a>
-                <br /> <br />
-                {/* <h3>Cincinnati</h3> */}
-                <p>Known for it's spaghetti chili, flying pigs, and world-famous hippos, Cincinnati is the place to be to experience a variety of cultures, art, food, and history. Click the dice below to learn some fun facts about our city. </p>
-                <br />
-                {/* {/* dice for fun fact generator will go here (add sound effect?)  */}
-                {/* <div>
-                 
-                    <button onClick={newFact}><FaDiceD20 /></button>
-                    <div id="factDisplay">{displayedFact}</div>
-                </div> <br /> */}
-
+                    <br /> <br />
+                    {/* <h3>Cincinnati</h3> */}
+                    <p>Known for it's spaghetti chili, flying pigs, and world-famous hippos, Cincinnati is the place to be to experience a variety of cultures, art, food, and history. Click the dice below to learn some fun facts about our city. </p>
+                    <br />
+                    {/* dice for fun fact generator*/}
+                    <div><FunFact /></div>
                     <a href="/about#section-three">
                         <FaArrowDown className="icons" />
                     </a>
--- src/components/FunFact.js
+++ src/components/FunFact.js
@@ -5,14 +5,14 @@ import Button from 'react-bootstrap/Button';
 const facts = [
   'Cincinnati was originally named Losantville. It was renamed in 1790 in honor of Lucius Cincinnatus. He was a military leader and statesman in Rome who lived from 519 to 430 B.C.E.',
   'Cincinnati is the birthplace of Neil Armstrong. He was born in Cincinnati in August 1930. After his victorious career with NASA, he went on to teach aeronautical engineering at the University of Cincinnati from 1971 to 1979.',
-  'The Cincinnati Observatory is the oldest professional observatory in the country and in the entire western hemisphere. It was established in 1842.',
+  'The Cincinnati Observatory was established in 1842 and is the oldest professional observatory in the country and in the entire western hemisphere.',
   'Cincinnati was a key stop in the Underground Railroad. Slaves escaping from the south could stop here on their way to Canada to be free. You can learn more on this subject at the National Underground Railroad Freedom Center right here in Cincinnati.',
   'Cincinnati is home to the biggest Oktoberfest in the country and second largest in the world with Germany holding the number one spot.',
   'Founded in 1895, the Cincinnati Symphony Orchestra is the sixth oldest orchestra in the country.',
   'Cincinnati is the home to the only ventriloquist museum in the world. The museum is called Vent Haven Museum and can only be visited via appointment.',
   'The Cincinnati Reds was the first Major League Baseball team in the country. Established in 1890, the team was originally named the Cincinnati Red Stockings.',
-  'Cincinnati is known as The Queen City but do you know its other nicknames? Cincinnati is also known as The City of Seven Hills, The Queen of the West, The Blue Chip City, and Porkopolis.',
-  'Cincinnati is known for its "strange" chili that includes cinnamon and chocolate in its seasoning. It is usually served on spagetti or hot dogs and piled high with cheese.', 
+  'Cincinnati is known as The Queen City but do you know its other nicknames? It is also known as The City of Seven Hills, The Queen of the West, The Blue Chip City, and Porkopolis.',
+  'Cincinnati is known for its chili that includes cinnamon and chocolate in its seasoning. It is usually served on spagetti or hot dogs and piled high with cheese.', 
   'Cincinnati is "the capital of Cornhole". Locals gave rise to the party game starting back in 2005.'
 ];
 
@@ -22,25 +22,31 @@ const FunFact = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => {
     const randomNumber = Math.floor(Math.random() * (facts.length));
-    setDisplayedFact(facts[randomNumber]).setShow(true);
+    setDisplayedFact(facts[randomNumber]);
+    setShow(true);
   }
-  //     const newFact =() =>{//     const randomNumber = Math.floor(Math.random() * (facts.length));
-  //     setDisplayedFact(facts[randomNumber])
-  //     // document.getElementById('factDisplay').innerHTML = facts[randomNumber];// }return (
+  
+  return (
   <div>
     <Button variant="primary" onClick={handleShow}>
       <FaDiceD20 className="icons" />
     </Button>
-    <Modal show={show} onHide={handleClose}>
+    <Modal id="modal-box"show={show} onHide={handleClose}>
+      <Modal.Header className="text-right">
+        <Button variant="tertiary" id="x-button" onClick={handleClose}>X</Button>
+      </Modal.Header>
       <Modal.Body className="text-center">{displayedFact}</Modal.Body>
       <Modal.Footer className="justify-content-center">
-        <Button variant="secondary" onClick={handleClose} >
-          Close
+      <div id="modal-icons">
+      {/* add hover over text that says: "roll again?" or something similar */}
+        <Button variant="secondary" onClick={handleShow} >
+          <FaDiceD20 />
         </Button>
+        </div>
       </Modal.Footer>
     </Modal>
   </div>
-
+  )
 }
 
 export default FunFact;
