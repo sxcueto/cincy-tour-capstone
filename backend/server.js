5db427fb9c0f251c642d5723a5359b54541761c6
unknown <sxcueto@gmail.com>
2023-06-08 20:32:10 -0400

    added jwt files

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/authRoutes');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +12,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set('view engine', 'ejs');
 
 // Routes
 // const artsRouter = require('../backend/routes/arts')
@@ -27,6 +36,11 @@ app.use(express.json());
 // app.use('/sports', sportsRouter)
    app.use('/attractions', attractionsRouter) 
 
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+// app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
@@ -34,7 +48,9 @@ app.listen(PORT, () => {
 
 // Connect to MongoDB
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true,})
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
 const connection = mongoose.connection;
 connection.once('open', () =>{
     console.log("MongoDB database connection established succesfully");
